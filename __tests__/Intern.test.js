e3ff16685b82583021e14c13a1165825ec8a1ade
browniecharl <99853094+browniecharl@users.noreply.github.com>
2022-05-14 15:52:34 -0400

    added tests for intern

---
+++ __tests__/Intern.test.js
@@ -0,0 +1,14 @@
+const Intern = require ('../lib/Intern');
+
+test(' Will create object for Intern ', () => {
+    const intern = new Intern('Robert', 4993, 'browniecharl@gmail.com', 'browniecharl', 'MCCC');
+    expect(intern.school).toEqual(expect.any(String));
+});
+test(' Uses getSchool to get interns school ', () => {
+    const intern = new Intern('Robert', 4993, 'browniecharl@gmail.com', 'browniecharl', 'MCCC');
+    expect(intern.getSchool()).toEqual(expect.any(String));
+});
+test(' Uses getRole to get role of intern ', () => {
+    const intern = new Intern('Robert', 4993, 'browniecharl@gmail.com', 'browniecharl', 'MCCC');
+    expect(intern.getRole()).toBe('Intern');
+});
