44e319a7bb2093da660ad29c29b9c233511e54e5
browniecharl <99853094+browniecharl@users.noreply.github.com>
2022-05-14 15:20:30 -0400

    made tests for engineer

---
+++ __tests__/Engineer.test.js
@@ -0,0 +1,14 @@
+const Engineer = require ('../lib/Engineer');
+
+test(' Will create object for engineer ', () => {
+    const engineer = new Engineer('Robert', 4993, 'browniecharl@gmail.com', 'browniecharl');
+    expect(engineer.github).toEqual(expect.any(String));
+});
+test(' Uses getGithub to get engineer GitHub username', () => {
+    const engineer = new Engineer('Robert', 4993, 'browniecharl@gmail.com', 'browniecharl');
+    expect(engineer.getGithub()).toEqual(expect.any(String));
+});
+test(' Uses getRole to display role of engineer ', () => {
+    const engineer = new Engineer('Robert', 4993, 'browniecharl@gmail.com', 'browniecharl');
+    expect(engineer.getRole()).toBe('Engineer');
+});
