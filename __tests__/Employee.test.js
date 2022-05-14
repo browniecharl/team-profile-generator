edd68160dfca61afabf57e861d670d59f0f92454
browniecharl <99853094+browniecharl@users.noreply.github.com>
2022-05-14 13:16:42 -0400

    added foo to new employee email and id to make the test pass

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -7,8 +7,20 @@ test('this will create the new employee object', () => {
 // Test get the employee name
 test('Gets the employee name', () => {
     const name = 'Robert';
-    const employee = new Employee('robert');
-    expect(employee.name).toBe('robert')
+    const employee = new Employee(name);
+    expect(employee.name).toBe(name)
+});
+//Test will get the employee ID
+test('Gets the employee ID', () => {
+    const id = 4993;
+    const employee = new Employee('Foo', id);
+    expect(employee.id).toBe(id)
+});
+// Test will get the employees email
+test('Gets the employees email', () => {
+    const email = 'browniecharl@gmail.com';
+    const employee = new Employee('Foo', 1, email);
+    expect(employee.email).toBe(email)
 });
 
 
