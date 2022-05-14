0b6dff10e8528474cca456bba5c610810a19a7e4
browniecharl <99853094+browniecharl@users.noreply.github.com>
2022-05-14 17:00:58 -0400

    started population index page with questions

---
+++ index.js
@@ -0,0 +1,12 @@
+const Intern = require('./lib/Intern');
+const Manager = require('./lib/Manager');
+const Engineer = require('./lib/Engineer');
+const fs = require('fs');
+const inquirer = require('inquirer');
+
+const teamMember = [];
+const questions = async () => {
+    const answers = await inquirer.prompt([
+        
+    ])
+}
